d60417c1f5ebce1033d655d950067cdd775c7956
Ian Zhang <ianzh4ng@gmail.com>
2021-08-21 11:43:15 -0700

    Setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const jsLoadingRules = {
+  test: /\.(ts|js)(x?)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader",
+  },
+};
+
+const cssLoadingRules = {
+  test: /\.mod\.scss|\.css$/,
+  use: [
+    "style-loader",
+    {
+      loader: "@teamsupercell/typings-for-css-modules-loader",
+      options: {
+        formatter: "prettier",
+      },
+    },
+    {
+      loader: "css-loader",
+      options: {
+        importLoaders: 1,
+        modules: {
+          localIdentName: "[local]__[hash:base64:5]",
+        },
+      },
+    },
+    "sass-loader",
+  ],
+};
+
+const imageLoadingRules = {
+  test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  type: "asset/resource",
+};
+
+const fontLoadingRules = {
+  test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  type: "asset/resource",
+};
+
+module.exports = {
+  entry: "./index.tsx",
+  // options related to how webpack emits results
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  // options for resolving module requests
+  resolve: {
+    // directories where to look for modules, in order
+    modules: [path.resolve(__dirname), "node_modules"],
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  // configuration regarding modules
+  module: {
+    rules: [
+      jsLoadingRules,
+      cssLoadingRules,
+      imageLoadingRules,
+      fontLoadingRules,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+};
