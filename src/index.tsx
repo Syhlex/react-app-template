884520be42a3b30c89e454b86d81be383ee112fe
Ian Zhang <ianzh4ng@gmail.com>
2022-12-02 21:58:00 -0800

    Add more folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import { App } from './App';
 
-ReactDOM.render(<div>My Application</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
