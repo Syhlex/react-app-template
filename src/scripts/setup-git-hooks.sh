13cfcf896b1082526c032fc21879a94ff4e6ae38
Ian Zhang <ianzh4ng@gmail.com>
2023-06-10 21:41:06 -0700

    Change hooks to git-hooks

--- src/scripts/setup-git-hooks.sh
+++ src/scripts/setup-git-hooks.sh
@@ -5,5 +5,5 @@ if [ -d ".git/hooks" ]; then
   rm -r ".git/hooks"
 fi
 
-# Create a symbolic link from .git/hooks that points to src/hooks
-ln -s ../src/hooks .git/hooks
+# Create a symbolic link from .git/hooks that points to src/git-hooks
+ln -s ../src/git-hooks .git/hooks
