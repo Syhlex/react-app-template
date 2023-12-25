bb07f4e79eca9144a095c997fb878de242fad4fb
Ian Zhang <ianzh4ng@gmail.com>
2023-12-24 23:40:42 -0800

    Upgrade React to v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './css';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+if (!rootEl) {
+  throw new Error('Unable to find element with id "root".');
+}
+
+const root = createRoot(rootEl);
+root.render(<App />);
