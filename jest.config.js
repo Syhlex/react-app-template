87d47a1fa72cd1793acd1bf87c4fb5f1a93bbb95
Ian Zhang <ianzh4ng@gmail.com>
2025-03-29 17:23:48 -0700

    Fix Jest being unable to parse CSS imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 export default {
   moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy', // https://jestjs.io/docs/webpack#mocking-css-modules
+  },
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [
     '/node_modules/(?!(lodash-es)/)', // https://jaketrent.com/post/jest-unexpected-token-typescript/
